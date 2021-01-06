6cdaf01dbb6f69cf83cf6083ce097136fe50f238
Dániel Losonczi <dlosonczi1@gmail.com>
2021-01-06 21:22:35 +0100

    display 3.0

--- Z0mzi math.cs
+++ Z0mzi math.cs
@@ -15,5 +15,10 @@ namespace Z0mziMath
         }
         public int x;
         public int y;
+
+        public override string ToString()
+        {
+            return string.Format($"{x}, {y}");
+        }
     }
 }
